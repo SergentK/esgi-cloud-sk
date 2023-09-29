49be9b61289e1d05cd06c701d5b11d429cd8d6b5
SergentKarote <Pouradier.clement@gmail.com>
2023-09-29 12:07:39 +0200

    Je m'en fou

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from 'firebase/app'
+
+const app = initializeApp({
+  apiKey: "AIzaSyD9f7UHqk2snXeuk1RcKlTi4jLNHb6hSgU", 
+  authDomain: "esgi-cloud-sk.firebaseapp.com",
+  //databaseURL: "", // TODO
+  projectId: "esgi-cloud-sk",
+  //storageBucket: "" // TODO
+})
+
+export default app
